1b074f7eff9c2cb3f2ba232f993083f93d5840b4
Gregory Sánchez <gsan0627@gmail.com>
2020-08-14 11:34:42 -0400

    docs agregado

---
+++ docs/precache-manifest.5dff5fa31e52a49090e57a9b188dd81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c2fcf4c7b196ec53da1075561900be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ac742d786e9f7e72c0",
+    "url": "/static/css/main.002fd7ae.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ac742d786e9f7e72c0",
+    "url": "/static/js/main.c5bb9985.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
